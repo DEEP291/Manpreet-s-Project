2ebb8decdc1c1b52575de9b6e2cd85057d264567
DEEP <srandeepkiran@gmail.com>
2021-12-06 14:15:06 -0500

    Initial Commit

---
+++ Shared/Manpreet_s_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manpreet_s_ProjectApp.swift
+//  Shared
+//
+//  Created by kiran deep on 2021-12-06.
+//
+
+import SwiftUI
+
+@main
+struct Manpreet_s_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
